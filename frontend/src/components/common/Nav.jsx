7475860bb7bbc82f5d974b3c8ebb3738bbc2a073
CharlesCryer <charliecac201@gmail.com>
2024-04-13 15:58:43 +1200

    added link to homepage in navbar

--- frontend/src/components/common/Nav.jsx
+++ frontend/src/components/common/Nav.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import NavStyles from "../../modules/common/Nav.module.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <>
-      <div className={NavStyles.nav_bar}>
-        <h1>Flashcards website</h1>
-      </div>
+      <Link to={"/"}>
+        <div className={NavStyles.nav_bar}>
+          <h1>Flashcards website</h1>
+        </div>
+      </Link>
     </>
   );
 };
